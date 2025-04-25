816663b69b15728f15955197abd17bbb6bb0afb1
Luke Huisman <huisman.l@yahoo.com>
2025-04-25 10:14:58 +0200

    Update jobs.blade.php

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -5,7 +5,7 @@
 
     <div class="space-y-4">
         @foreach($jobs as $job)
-            <a href="jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+            <a href="jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg shadow-md">
                 <div class="text-blue-500 font-bold text-sm">{{ $job->employer->name }}</div>
                 <div>
                     <strong>{{ $job['title'] }}</strong>: Pays: {{ $job['salary'] }} per year
