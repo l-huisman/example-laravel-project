225acabc17f0fdf5204b43037f8a0c27f693a32c
Luke Huisman <huisman.l@yahoo.com>
2025-04-25 10:08:28 +0200

    initial commit

---
+++ database/migrations/2025_04_20_135407_job_listings.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('job_listings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Employer::class);
+            $table->string('title');
+            $table->string('salary');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        //
+    }
+};
+
---
+++ resources/views/job.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+    <x-slot:heading>
+        Jobs Page
+    </x-slot:heading>
+
+    <h2>{{$job['title']}}</h2>
+
+    <p>
+        This job pays {{ $job['salary'] }} per year!
+    </p>
+</x-layout>
