fb4ca5e19fbcdb567873f9b27d34b8dbf40f7ddf
Luke Huisman <huisman.l@yahoo.com>
2025-04-29 09:22:11 +0200

    Handled CSRF (Cross-site request forgery)

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,32 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(5);
+    $jobs = Job::with('employer')->latest()->simplePaginate(5);
 
-    return view('jobs', [
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
