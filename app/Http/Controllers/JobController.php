28fc16e468714c061ff6f05f359be5ee758b05b8
Luke Huisman <huisman.l@yahoo.com>
2025-04-30 11:30:38 +0200

    Add authorization for editing jobs

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class JobController extends Controller
 {
