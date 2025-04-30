8b4d2f2ce96388cd8cd3aa76e2012abcdb7f4cf3
Luke Huisman <huisman.l@yahoo.com>
2025-04-30 10:29:24 +0200

    Add authentication views, routes, and reusable form components

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,18 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
-//Route::controller(JobController::class)->group(function () {
-//    Route::get('/jobs', 'index');
-//    Route::get('/jobs/create', 'create');
-//    Route::get('/jobs/{job}', 'show');
-//    Route::post('/jobs', 'store');
-//    Route::get('/jobs/{job}/edit', 'edit');
-//    Route::patch('/jobs/{job}', 'update');
-//    Route::delete('/jobs/{job}', 'destroy');
-//});
-
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
+
+Route::get('login', [LoginController::class, 'create']);
+Route::post('login', [LoginController::class, 'store']);
