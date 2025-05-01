924841397d4760a629c5ccc4c8f35179149136c0
Luke Huisman <huisman.l@yahoo.com>
2025-05-01 09:31:30 +0200

    Add job posting email notification functionality

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\JobPosted;
 use App\Models\Job;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 
 class JobController extends Controller
 {
@@ -30,11 +29,13 @@ class JobController extends Controller
             'salary' => ['required', 'numeric'],
         ]);
 
-        Job::create([
+        $job = Job::create([
             'title' => request('title'),
             'salary' => request('salary'),
             'employer_id' => 1,
         ]);
+
+        Mail::to($job->employer->user)->send(new JobPosted($job));
 
         return redirect('/jobs');
     }
